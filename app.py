c3379af89a2644c52d8fe7cbea2caafaf13d29e9
Archisha <archisha.singh@gmail.com>
2026-06-10 14:15:33 +0530

    Added code for affirmations page

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import random
+
+st.title("✨Daily affirmations✨")
+
+st.image("assets/thumbs_up.jpg", width=250)
+
+with open("data/affirmations.txt", r) as file:
+    affirmations = file.readlines()
+
+affirmations = [line.strip() for line in affirmations if line.strip()]
+
+affirmation = random.choice(affirmations)
+
+st.success(affirmation)
