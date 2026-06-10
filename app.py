52180c8ff8514cc23ec49c9f3b80f9d96a09cb87
Archisha <archisha.singh@gmail.com>
2026-06-10 14:41:42 +0530

    Fixed affirmation text issue and formatting

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import random
 
-st.title("✨Daily affirmations✨")
+st.title("<h1 style='text-align: center;'>Daily Affirmations ✨</h1>", unsafe_allow_html=True)
 
-st.image("assets/thumbs_up.jpeg", width=250)
+image_path = Path("assets/thumbs_up.jpg")
 
-with open("data/affirmations.txt", r) as file:
+if image_path.exists():
+    st.image(str(image_path), width=750, height=400)
+else:
+    st.warning("Image not found.")
+
+with open("data/affirmations.txt", "r") as file:
     affirmations = file.readlines()
 
 affirmations = [line.strip() for line in affirmations if line.strip()]
@@ -13,3 +18,19 @@ affirmations = [line.strip() for line in affirmations if line.strip()]
 affirmation = random.choice(affirmations)
 
 st.success(affirmation)
+
+st.markdown(
+    f"""
+    <div style="
+        text-align: center;
+        font-size: 28px;
+        padding: 30px;
+        margin-top: 20px;
+        border-radius: 20px;
+        background-color: #f5f5f5;
+    ">
+        {affirmation}
+    </div>
+    """,
+    unsafe_allow_html=True
+)
